c5291c800eedff6acedc28377300fa1aeef69836
boyloy <sivtyly9@gmail.com>
2025-07-07 18:32:12 +0800

    Version 1

---
+++ script/products.js
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    image: 1,
+    name: "Tulip",
+    priceCents: 200,
+  },
+  {
+    image: 2,
+    name: "Cat Flower",
+    priceCents: 300,
+  },
+  {
+    image: 3,
+    name: "Cherry Blossom",
+    priceCents: 550,
+  },
+  {
+    image: 4,
+    name: "Lotus",
+    priceCents: 100,
+  },
+  {
+    image: 5,
+    name: "Bellflower",
+    priceCents: 300,
+  },
+  {
+    image: 6,
+    name: "Rose",
+    priceCents: 490,
+  },
+  {
+    image: 7,
+    name: "Lili",
+    priceCents: 400,
+  },
+  {
+    image: 8,
+    name: "Sun Flower",
+    priceCents: 1000,
+  },
+];
